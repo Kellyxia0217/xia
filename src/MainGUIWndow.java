260ea6fff3e5d38e91aa6f41bd16820de65a99fa
unknown <BT_3W2_33@BKTECH.LOCAL>
2023-05-02 09:30:24 -0400

    Initial commit

---
+++ src/MainGUIWndow.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MainGUIWndow {
+    private JTextArea billTextArea;
+    private JTextArea tipTextArea1;
+    private JTextArea tipTextArea;
+    private JTextArea numberOfPeopleTextArea;
+    private JTextArea textArea1;
+    private JPanel mainPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
